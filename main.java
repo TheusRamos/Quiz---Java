6c65c9c92ae0efbc6f8a6bafe13ddd3a463bbf82
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-03-24 16:58:40 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,18 @@
 package quiz;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
+        BufferedReader leitura = new BufferedReader(new FileReader("arquivo_quiz.csv"));
         
+        
+        
+        
+        
+        
+        
+        leitura.close();
     }    
 }
