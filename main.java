bc7453566621de92a7d0e71e67c861cef26ac129
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-03-24 16:47:43 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package quiz;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
